c10fcbb3c1cf87086fcd87c5d9eb4146dba2732e
Rohit Walia <waliarohit777@gmail.com>
2021-02-15 23:55:15 +0530

    Part 3: Failing test case

--- src/test/java/RestaurantTest.java
+++ src/test/java/RestaurantTest.java
@@ -45,9 +45,8 @@ class RestaurantTest {
     }
     @Test
     public void calculateOrderTotal_should_return_400_after_adding_the_350_and_50(){
-        addRestaurantDetails();
         Integer orderTotal = restaurant.calculateOrderTotal(350,50);
-        assertEquals(400,orderTotal);
+        assertEquals(400,restaurant.calculateOrderTotal());
     }
 
     //<<<<<<<<<<<<<<<<<<<<<<<<<OPEN/CLOSED>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
